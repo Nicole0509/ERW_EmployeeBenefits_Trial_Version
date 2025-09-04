d58b48323f8f8c2e02d87eb6aeae674bb1c5554b
Nicole0509 <niashavril0509@gmail.com>
2025-09-04 21:57:02 +0200

    created The employee table and its factory

---
+++ EmployeeBenefitsManagement/database/migrations/2025_09_04_192411_create_employees_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('employees', function (Blueprint $table) {
+            $table->id();
+            $table->string('employeeID')->nullable(false)->unique();
+            $table->string('name')->nullable(false);
+            $table->string('department')->nullable(false);
+            $table->string('role')->nullable(false);
+            $table->string('location')->nullable(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('employees');
+    }
+};
